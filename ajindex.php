74d3c9697793940ecac56e196d7100dd0398d5c3
Aluer85 <75521485+Aluer85@users.noreply.github.com>
2020-12-18 16:57:14 +0300

    Add files via upload

--- ajindex.php
+++ ajindex.php
@@ -1,19 +1,9 @@
 <?php
-//session_start();
-//echo session_id();
-?>
-<?php
-/*if(isset($_POST)) {
-	foreach($_POST as $k => $v) {
-		echo 'POST!!!';
-		}
-	}*/
 	
 if(isset($_GET)) {
 	foreach($_GET as $k => $v) {$$k = $v;}
 	}
 	if(isset($_FILES['files']['tmp_name'])) {
-		echo 'test1';
 		//var_dump($_FILES);
 		//move_uploaded_file($_FILES['image']['tmp_name'], $_FILES['image']['name']);
 		}
